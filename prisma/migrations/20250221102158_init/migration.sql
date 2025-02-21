1eb1aa6d2abb7ac61ff47f2b486ebae2d03afd19
tah <fantianl07@gmail.com>
2025-02-21 19:22:06 +0900

    Add initial database migration scripts for GlobalSettings, DataStoreApiKey, Game, Rule, History, HistoryRules, and ErrorLog tables

---
+++ prisma/migrations/20250221102158_init/migration.sql
@@ -0,0 +1,64 @@
+-- CreateTable
+CREATE TABLE "GlobalSettings" (
+    "id" TEXT NOT NULL PRIMARY KEY DEFAULT 'settings',
+    "webhookAuthKey" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "DataStoreApiKey" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "label" TEXT NOT NULL,
+    "apiKey" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "label" TEXT NOT NULL,
+    "universeId" BIGINT NOT NULL,
+    "startPlaceId" BIGINT NOT NULL,
+    "apiKeyId" TEXT NOT NULL,
+    CONSTRAINT "Game_apiKeyId_fkey" FOREIGN KEY ("apiKeyId") REFERENCES "DataStoreApiKey" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Rule" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "gameId" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "datastoreName" TEXT NOT NULL,
+    "datastoreType" TEXT NOT NULL,
+    "keyPattern" TEXT NOT NULL,
+    "scope" TEXT NOT NULL,
+    CONSTRAINT "Rule_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "History" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "History_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "HistoryRules" (
+    "historyId" TEXT NOT NULL,
+    "ruleId" TEXT NOT NULL,
+
+    PRIMARY KEY ("historyId", "ruleId"),
+    CONSTRAINT "HistoryRules_historyId_fkey" FOREIGN KEY ("historyId") REFERENCES "History" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "HistoryRules_ruleId_fkey" FOREIGN KEY ("ruleId") REFERENCES "Rule" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ErrorLog" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "error" TEXT NOT NULL,
+    "gameId" TEXT,
+    "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DataStoreApiKey_label_key" ON "DataStoreApiKey"("label");
